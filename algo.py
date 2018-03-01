22304644092f1ed3226d7d46f5ec1ab98cba27ba
Théo VIEL <theo.viel@eleves.enpc.fr>
2018-03-01 19:47:47 +0100

    fin classes

--- algo.py
+++ algo.py
@@ -16,6 +16,8 @@ def time_travel(end, start):
     
 class travel:
     def __init__(self, infos):
+        self.index = compt
+        
         self.pt_start=infos[0]
         self.pt_end=infos[1]
         self.earliest_start= infos[2]
@@ -28,13 +30,19 @@ class travel:
         
         self.done= False
         self.bonus=False
-
-
     
-class voiture:
+    def reset(self):
+        self.done = False
+        self.bonus = False 
+        self.time_start= -1
+        self.time_finish = -1
+
+
+class car:
     def __init__(self):
         self.pos=(0 , 0)
         self.time_avail = 0
+        self.affectations = affectation()
     
     
     def assign_travel(self, travel):
@@ -47,6 +55,48 @@ class voiture:
         travel.done=True
         if travel.time_start==travel.earliest_start:
             travel.bonus= True
+        
+        self.affectations.affect(travel)
+    
+    
+    def reset(self):
+        self.pos= (0,0)
+        self.time_avail=0
+
+
+class affectation:
+    def __init__(self):
+        list=[0]
+    
+    def reset(self):
+        affectations=[]
+        
+    def affect(self, travel):
+        self.list[0]+=1
+        self.list.append(travel.index)
+
+
+def reset_all():
+    for travel in travels:
+        travel.reset()
+    for car in cars:
+        car.reset()
+
+
+def soonest_available(cars):
+    min = 1000000000
+    for car in cars:
+        if car.time_avail<=min:
+            best_car= car
+            min = car.time_avail
+    return car
+            
+
+def assign_random(cars, travels):
+    for travel in travels:
+        car = soonest_available(cars)
+        car.assign_travel(travel)
+
         
 
 
